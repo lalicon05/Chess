ba368846fb92013a75929d1c42b5b7d515b6cded
lalicon05 <lasse.l535@gmail.com>
2026-03-27 11:15:41 +0100

    it is almost time to generate random moves. but I don't have time right now.

---
+++ chess_engine/src/dennis_engine.rs
@@ -0,0 +1,22 @@
+// File that will contain Dennis the chess engine
+
+
+use rand::seq::SliceRandom;
+use rand::thread_rng;
+
+use crate::game::Game;
+
+pub struct DennisEngine;
+
+impl DennisEngine {
+    pub fn pick_move(game: &Game) -> Option<Move> {
+        let moves = game.generate_legal_moves();
+
+        if moves.is_empty() {
+            return None;
+        }
+
+        let mut rng = thread_rng();
+        moves.choose(&mut rng).copied()
+    }
+}
